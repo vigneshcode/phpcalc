9f195d1b9209f8fb1a56982dee93f64081cbba3d
vigneshwaran <in.vignesh@gmail.com>
2018-12-27 19:50:28 -0500

    Initial

---
+++ JS/MOS.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+{
+"enlisted":[
+    { "MOS":"00Z","Title:Sergeant Major Nominative","Cat":"M"},
+    { "MOS":"00Z","Title:Sergeant Major Nominative","Cat":"M"},
+    { "MOS":"00Z","Title:Sergeant Major Nominative","Cat":"M"}
+]
+};
+
+{
+    "DeadLift":
+}
