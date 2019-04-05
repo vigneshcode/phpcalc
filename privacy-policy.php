404d74ff428170b6a1356e7c05d6f51ead98fcde
vigneshwaran <in.vignesh@gmail.com>
2019-04-05 06:34:00 +0530

    added facebook

--- privacy-policy.php
+++ privacy-policy.php
@@ -27,7 +27,16 @@
                     accessible at ACFT Calculator unless otherwise defined in this Privacy Policy.
                   </p> <p><strong>Information Collection and Use</strong></p> <p>For a better experience, while using our Service, I may require you to provide us with certain
                     personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way.
-                  </p> <p>The app does use third party services that may collect information used to identify you.</p> <div><p>Link to privacy policy of third party service providers used by the app</p> <ul><li><a href="https://www.google.com/policies/privacy/" target="_blank">Google Play Services</a></li><li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank">AdMob</a></li><li><a href="https://firebase.google.com/policies/analytics" target="_blank">Firebase Analytics</a></li><!----><li><a href="https://fabric.io/privacy" target="_blank">Fabric</a></li><li><a href="https://firebase.google.com/terms/crashlytics/" target="_blank">Crashlytics</a></li><!----><!----></ul></div> <p><strong>Log Data</strong></p> <p> I want to inform you that whenever you use my Service, in a case of
+                  </p> <p>The app does use third party services that may collect information used to identify you.</p> <div><p>Link to privacy policy of third party service providers used by the app</p> 
+                      <ul><li><a href="https://www.google.com/policies/privacy/" target="_blank">Google Play Services</a></li>
+                          <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank">AdMob</a></li>
+                          <li><a href="https://firebase.google.com/policies/analytics" target="_blank">Firebase Analytics</a>
+                          </li><!----><li><a href="https://fabric.io/privacy" target="_blank">Fabric</a></li>
+                          <li><a href="https://firebase.google.com/terms/crashlytics/" target="_blank">Crashlytics</a>
+                              
+                          </li>
+                          <li><a href="https://www.facebook.com/about/privacy" target="_blank">Facebook</a></li>
+                          <!----><!----></ul></div> <p><strong>Log Data</strong></p> <p> I want to inform you that whenever you use my Service, in a case of
                     an error in the app I collect data and information (through third party products) on your phone
                     called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address,
                     device name, operating system version, the configuration of the app when utilizing my Service,
@@ -39,7 +48,11 @@
                     libraries that use “cookies” to collect information and improve their services. You have the option to
                     either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose
                     to refuse our cookies, you may not be able to use some portions of this Service.
-                  </p> <p><strong>Service Providers</strong></p> <p> I may employ third-party companies and individuals due to the following reasons:</p> <ul><li>To facilitate our Service;</li> <li>To provide the Service on our behalf;</li> <li>To perform Service-related services; or</li> <li>To assist us in analyzing how our Service is used.</li></ul> <p> I want to inform users of this Service that these third parties have access to
+                  </p> <p><strong>Service Providers</strong></p> <p> I may employ third-party companies and individuals due to the following reasons:</p> 
+                  <ul><li>To facilitate our Service;</li> <li>To provide the Service on our behalf;</li> 
+                      <li>To perform Service-related services; or</li>
+                      <li>To assist us in analyzing how our Service is used.</li></ul> 
+                  <p> I want to inform users of this Service that these third parties have access to
                     your Personal Information. The reason is to perform the tasks assigned to them on our behalf. However,
                     they are obligated not to disclose or use the information for any other purpose.
                   </p> <p><strong>Security</strong></p> <p> I value your trust in providing us your Personal Information, thus we are striving
